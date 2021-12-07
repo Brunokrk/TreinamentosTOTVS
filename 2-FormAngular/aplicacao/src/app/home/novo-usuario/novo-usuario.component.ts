f3400655ca27a903f903d3584d1b51e3223ebc4d
Bruno Marchi Pires <piresbr@outlook.com>
2021-12-07 13:45:55 -0300

    .

--- 2-FormAngular/aplicacao/src/app/home/novo-usuario/novo-usuario.component.ts
+++ 2-FormAngular/aplicacao/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,11 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './ususario-senha-iguais.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -11,7 +15,12 @@ import { NovoUsuario } from './novo-usuario';
 export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder, private NovoUsuarioService: NovoUsuarioService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private NovoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService,
+    private router:Router
+    ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -21,14 +30,27 @@ export class NovoUsuarioComponent implements OnInit {
       fullName:['',[
         Validators.required, Validators.minLength(4)
       ]],
-      userName:[''],
+      userName:['',[minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],
       password:[''],
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
     });
   }
 
   cadastrar(){
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario)
+    if(this.novoUsuarioForm.valid){
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.NovoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(()=>{
+        this.router.navigate(['']);
+      },
+        (error)=>{
+          console.log(error)
+        }
+      );
+    }
+
+
   }
 
 }
